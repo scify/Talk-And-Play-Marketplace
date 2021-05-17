b192e6d59f7fa0e839b81c627be8371861cf71ea
Paul Isaris <paulisaris@gmail.com>
2021-05-17 11:08:29 +0300

    Main logo

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="{{ route('home') }}" tabindex="-1">
+            <img src="{{ asset('img/tp_logo_small.png') }}" height="50px" alt="Marketplace logo">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
