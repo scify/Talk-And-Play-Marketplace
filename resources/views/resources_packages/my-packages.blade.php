a8565c99bd32396f67f4669ee43c5d0f09f5ead3
ngialitsis <ngialitsis@scify.gr>
2021-09-02 10:53:42 +0300

    TPM 129 | CSS changes

--- resources/views/resources_packages/my-packages.blade.php
+++ resources/views/resources_packages/my-packages.blade.php
@@ -6,7 +6,7 @@
 @section('content')
     <div class="container rounded py-4" align="center" style="border:1px solid grey;margin:auto;width:60%">
         <div class="mx-3">
-            <b>{{trans("messages.communication_cards")}}</b>
+            <h2>{{trans("messages.communication_cards")}}</h2>
         </div>
         <div class="row mt-5">
             <resources-packages-with-filters
@@ -16,9 +16,9 @@
                 :packages-type="'COMMUNICATION'">
             </resources-packages-with-filters>
         </div>
-        <hr>
-        <div class="mx-3">
-            <b>{{trans("messages.game_cards")}}</b>
+        <hr style="height:12px;border-width:0;color:gray;background-color:red">
+        <div class="mx-3 mt-3 mb-3">
+            <h2>{{trans("messages.game_cards")}}</h2>
         </div>
         <div class="row mt-5">
             <div class="col">
