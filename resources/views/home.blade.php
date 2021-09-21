5af0230f216ea7049f9251e1971e9346281888fc
NikolasGialitsis <nikolasyal@gmail.com>
2021-09-21 09:40:04 +0300

    css changes for download TnP desktop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,13 +43,13 @@
                                             <div class="row">
                                                 <div class="col">
                                                     <a href="https://go.scify.gr/talkandplaydownloadw"
-                                                       target="_blank" class="btn btn-primary w-100">
+                                                       target="_blank" class="btn btn-primary w-100" style="height:auto">
                                                         {!! __('messages.download_the_app') !!} <i class="ml-1 fab fa-windows"></i>
                                                     </a>
                                                 </div>
                                                 <div class="col">
                                                     <a href="https://go.scify.gr/talkandplaydownloadl"
-                                                       target="_blank" class="btn btn-primary w-100">
+                                                       target="_blank" class="btn btn-primary w-100" style="height:auto">
                                                         {!! __('messages.download_the_app') !!} <i class="ml-1 fab fa-linux"></i>
                                                     </a>
                                                 </div>
