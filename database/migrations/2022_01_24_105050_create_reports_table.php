88f091a82a273337a96acadea114f5e645a5be51
NikolasGialitsis <nikolasyal@gmail.com>
2022-01-24 12:59:24 +0200

    create reports table

---
+++ database/migrations/2022_01_24_105050_create_reports_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('package_id');
+            $table->foreign('package_id')->references('id')->on('resources_package');
+
+            $table->unsignedBigInteger('reporting_user_id');
+            $table->foreign('reporting_user_id')->references('id')->on('users');
+
+            $table->string('reason');
+            $table->string('comment');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
