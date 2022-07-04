e8530938524d1c5ecbdeff81742e33c0dc37aa9b
Paul Isaris <paulisaris@gmail.com>
2022-07-04 14:35:14 +0300

    Added API route to get Desktop app options

--- app/Http/Controllers/DesktopAppController.php
+++ app/Http/Controllers/DesktopAppController.php
@@ -8,7 +8,8 @@ class DesktopAppController extends Controller {
 
     public function getOptionsForDesktopApp(): JsonResponse {
         return response()->json([
-            'shapes_auth_url' => 'https://kubernetes.pasiphae.eu/shapes/asapa/auth/',
+            'shapes_auth_url_login' => 'https://kubernetes.pasiphae.eu/shapes/asapa/auth/login',
+            'shapes_auth_url_register' => 'https://kubernetes.pasiphae.eu/shapes/asapa/auth/register',
             'shapes_x_key' => config('app.shapes_key'),
             'sentry_dsn' => config('app.sentry_desktop_app_dsn'),
             'firebase_url' => config('app.firebase_desktop_app_url')
