fd1f3c89f0cf8fbe37f8c62f4b2bd36fb2761b3d
Paul Isaris <paulisaris@gmail.com>
2022-10-10 12:52:09 +0300

    Made response nullable

---
+++ database/migrations/2022_10_10_095129_make_response_nullable.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeResponseNullable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('analytics_events', function (Blueprint $table) {
+            $table->json('response')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        //
+    }
+}
